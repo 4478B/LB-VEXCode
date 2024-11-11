1a9bce8706fe2965666fa6b614ea3b5e1488c9af
Evan Boyle <evan.wesley.boyle@gmail.com>
2024-11-11 16:07:15 -0500

    created odometry functions

--- src/main.cpp
+++ src/main.cpp
@@ -59,6 +59,134 @@ digital_out sintake = digital_out(Brain.ThreeWirePort.D);
 // Limit switch declarations
 limit limitS = limit(Brain.ThreeWirePort.E);
 
+// Radius of parallel wheels
+const double WHEEL_RADIUS = 1.379;
+
+// distance between right wheel and tracking center [inches?]
+rDist = 5;
+
+// initial angle robot starts at (specified in odomSelector) [radians]
+double initTheta = 0;
+
+// positions of parallel wheels used in odometry loop [inches]
+double currentR = 0;
+double prevR = 0;
+
+// displacement of wheel between loops [inches]
+double deltaR = 0;
+
+// angles of bot used in odometry loop [radians]
+double theta;
+double prevTheta;
+
+// change in angle of bot between loops [radians]
+double deltaTheta = 0;
+
+
+// average theta throughout arc in each loop [radians]
+double avgTheta;
+
+// local displacement in each loop [inches]
+double deltaLocal = 0;
+
+// local displacement mapped to (x,y) [inches]
+double deltaXGlobal = 0;
+double deltaYGlobal = 0;
+
+// global position of the robot [inches]
+double posX;
+double posY;
+
+// variable representing if odometry is running
+bool odomRunning = false;
+// variable representing if odometry is being calculated
+bool odomLoopActive = false;
+
+void odometry() {
+  odomRunning = true;
+
+  while(odomRunning) {  
+    odomLoopActive = true;
+
+    // Gets the angle of the right wheel [radians]
+    currentR = mMidRight.position(deg) * Math.pi / 180;
+    // calculates change in wheel position since last loop [inches]
+    deltaR = (currentR - prevR) * WHEEL_RADIUS;
+    // Updates positions for next loop
+    prevR = currentR;
+
+    // Gets the orientation of robot [radians]
+    theta = Inertial.heading() * math.pi / 180;
+    // Wraps theta around domain of [0,2pi)
+    if(theta < 0 || theta > 2 * M_PI){
+      theta = fmod(theta,2*M_PI);
+    }
+
+    // Calculates change in orientation since last loop [radians]
+    deltaTheta = theta - prevTheta;
+    // Updates positions for next loop
+    prevTheta = theta;
+
+    if(deltaTheta == 0) {
+      // If robot didn't turn, movement is straightforward
+      deltaLocal = deltaR;
+    } else {
+      // Otherwise, use law of sines to solve for movement
+      deltaLocal = 2 * ((deltaR / deltaTheta) - rDist) * sin(deltaTheta / 2.0);
+    }
+
+    // Calculates average angle of theta in arc (loop) [radians]
+    avgTheta = theta - (deltaTheta / 2);
+
+    // Splits local displacement into X and Y factors
+    deltaXGlobal = (deltaLocal * cos(avgTheta));
+    deltaYGlobal = (deltaLocal * sin(avgTheta));
+
+    //* Updates the global position
+    posX += deltaXGlobal;
+    posY += deltaYGlobal;
+
+    odomLoopActive = false;
+    wait(10,msec)
+  }
+}
+
+// sets starting values based on auton configs
+void setStartingOdomValues() {
+  switch (autonSelect) {
+      case 0:
+        // WIP
+        posX = 0;
+        posY = 0;
+        initTheta = 0;
+        break;
+  }
+  theta = initTheta;
+  prevTheta = theta;
+  avgTheta = theta + (deltaTheta / 2);
+}
+
+void odomKillSwitch() {
+  odomRunning = !odomRunning;
+}
+
+void odomDataCollection() {
+  printf("%s | %s | %s | %s | %s \n",
+    center("theta",14).c_str(), center("deltaTheta",14).c_str(), center("avgTheta",14).c_str(),
+    center("posX",14).c_str(), center("posY",14).c_str());
+  
+  printf("%s \n", std::string(15*8 + 2*8, '-').c_str());
+
+  while(odomRunning) {
+    printf("%s | %s | %s | %s | %s \n",
+      prd(radToDeg(theta),2,14).c_str(),
+      prd(deltaTheta,2,14).c_str(), prd(avgTheta,2,14).c_str(), prd(posX,2,14).c_str(),
+      prd(posY,2,14).c_str());
+
+    wait(500, msec);
+  }
+}
+
 void printCenter(String input){
   // Screen Size (x,y) = (480, 240)
   int width = Brain.Screen.getStringWidth(input);
